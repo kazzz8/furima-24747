47ca402d059faea13c9ecd65da5aa1c485e4de62
kazzz8 <wmat441@gmail.com>
2023-10-08 16:05:59 +0900

    出品画面2 手数料と利益表示

---
+++ app/javascript/item_price.js
@@ -0,0 +1,9 @@
+const priceInput = document.getElementById("item-price");
+const addTaxDom = document.getElementById("add-tax-price");
+const profitDom = document.getElementById("profit")
+
+priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+  addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+  profitDom.innerHTML = Math.ceil(inputValue * 0.9)
+})
