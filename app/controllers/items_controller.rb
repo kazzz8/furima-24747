f1a9fc1d146120987a7171f8a50d3763e24c270c
kazzz8 <wmat441@gmail.com>
2023-10-20 21:47:34 +0900

    レビュー後修正1
    インスタンス変数をまとめた

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,10 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -19,18 +24,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    return if @item.user.id == current_user.id
-
-    redirect_to action: :index
+    unless @item.user.id == current_user.id
+      redirect_to action: :index
+    end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(params[:id])
     else
