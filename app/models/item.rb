0fa6a7a625cd7f988618e52bc277e2f261190882
kazzz8 <wmat441@gmail.com>
2023-10-28 12:02:52 +0900

    商品購入ページの表示内容作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,8 @@ class Item < ApplicationRecord
   # Association
   belongs_to :user
   has_one_attached :image
+  has_one :order
+  has_many :orders
 
   # ActiveHash
   extend ActiveHash::Associations::ActiveRecordExtensions
