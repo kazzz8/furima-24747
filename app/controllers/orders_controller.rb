f6e17f905430dc25e013c25eb13941d572b25fef
kazzz8 <wmat441@gmail.com>
2023-10-28 15:25:10 +0900

    クレジットカード決済機能途中やめ

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,15 @@ class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
   end
+
+  def create
+    
+  end
+
+  private
+
+  def order_params
+    params.require(:order).merge(token: params[:token])
+  end
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,9 @@
 class Order < ApplicationRecord
+
+
+  attr_accessor :token
+
+  # アソシエーション
   belongs_to :user
   belongs_to :item
   # has_one :delivery_info
